1a9bb4edc5b2c91606742daf895004a742e080fe
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-10-10 23:09:29 -0300

    Test: Hello World

--- src/ola-mundo.Test/OlaMundoTest.cs
+++ src/ola-mundo.Test/OlaMundoTest.cs
@@ -1,15 +1,18 @@
 using ola_mundo;
 using System.IO;
 using System;
+using Xunit;
+using FluentAssertions;
 
 namespace ola_mundo.Test;
 
 public class OlaMundoTest
 {
     [Theory(DisplayName = "Deve exibir Olá, Mundo!")]
-    [InlineData("<Insira algo aqui>")]
+    [InlineData("Olá, Mundo!")]
     public void TestOlaMundo(string Expected)
     {        
-        throw new notImplementedException();                    
+        var result = HelloWorld.Greet();
+        result.Should().Be(Expected);
     }
 }
